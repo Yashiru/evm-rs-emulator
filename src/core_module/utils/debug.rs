765ffb95eb73da4396334d4ddead2ea563651f65
Léo FASANO <fasanol.info@gmail.com>
2023-07-12 19:01:27 +0200

    feat: add vec to hex string utils

--- src/core_module/utils/debug.rs
+++ src/core_module/utils/debug.rs
@@ -10,3 +10,13 @@ pub fn to_hex_string(bytes: [u8; 32]) -> String {
     .collect::<Vec<String>>()
     .join(" ")
 }
+
+pub fn vec_to_hex_string(bytes: Vec<u8>) -> String {
+    bytes.iter()
+    .map(|b| match format!("{:02x}", b) {
+        s if s == "00" => s.truecolor(80, 80, 80).to_string(),
+        s => s.green().to_string(),
+    })
+    .collect::<Vec<String>>()
+    .join(" ")
+}
