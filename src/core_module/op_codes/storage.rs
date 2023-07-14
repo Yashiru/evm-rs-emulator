1c56833078106fd7f530bdb4dc9a9c86338c0e4b
Léo FASANO <fasanol.info@gmail.com>
2023-07-15 00:11:55 +0200

    test: add storage OpCodesTests

--- src/core_module/op_codes/storage.rs
+++ src/core_module/op_codes/storage.rs
@@ -55,3 +55,34 @@ pub fn sstore(runner: &mut Runner) -> Result<(), ExecutionError> {
     // Increment PC
     runner.increment_pc(1)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::core_module::runner::Runner;
+    use crate::core_module::utils::bytes::{_hex_string_to_bytes, pad_left};
+    use crate::core_module::utils::errors::ExecutionError;
+
+    #[test]
+    fn test_sload() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("602e600055600054600154"), Some(5), true);
+        assert!(interpret_result.is_ok());
+
+        let result: [u8; 32] = unsafe { runner.stack.pop().unwrap() };
+        assert_eq!(result, pad_left(&[0x00]));
+        let result: [u8; 32] = unsafe { runner.stack.pop().unwrap() };
+        assert_eq!(result, pad_left(&[0x2e]));
+    }
+
+    #[test]
+    fn test_sstore() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("602e600055"), Some(5), true);
+        assert!(interpret_result.is_ok());
+
+        let result = runner.state.sload(runner.address, pad_left(&[0x00])).unwrap();
+        assert_eq!(result, pad_left(&[0x2e]));
+    }
+
+}
