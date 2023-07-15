28120c8a832f8a40ece60e687121b444b99b3d8c
Léo FASANO <fasanol.info@gmail.com>
2023-07-15 17:21:56 +0200

    test: add logs OpCodes tests

--- src/core_module/op_codes/log.rs
+++ src/core_module/op_codes/log.rs
@@ -1,4 +1,5 @@
 use crate::core_module::runner::Runner;
+use crate::core_module::state::Log;
 use crate::core_module::utils;
 use crate::core_module::utils::errors::ExecutionError;
 
@@ -16,12 +17,15 @@ pub fn log0(runner: &mut Runner) -> Result<(), ExecutionError> {
     let offset = U256::from_big_endian(&unsafe { runner.stack.pop()? });
     let size = U256::from_big_endian(&unsafe { runner.stack.pop()? });
 
-    // Check if the address is out of bounds
-    if offset.as_usize() + size.as_usize() > runner.memory.heap.len() {
-        return Err(ExecutionError::OutOfBoundsMemory);
-    }
-
     let log_data = unsafe { runner.memory.read(offset.as_usize(), size.as_usize())? };
+    
+    let log = Log {
+        address: runner.address,
+        topics: vec![],
+        data: log_data.clone(),
+    };
+    
+    runner.state.logs.push(log);
 
     if runner.debug_level.is_some() && runner.debug_level.unwrap() >= 1 {
         let hex = utils::debug::vec_to_hex_string(log_data);
@@ -70,12 +74,16 @@ pub fn log1(runner: &mut Runner) -> Result<(), ExecutionError> {
     // print size
     println!("size: {:?}", size);
 
-    // Check if the address is out of bounds
-    if offset.as_usize() + size.as_usize() > runner.memory.heap.len() {
-        return Err(ExecutionError::OutOfBoundsMemory);
-    }
-
     let log_data = unsafe { runner.memory.read(offset.as_usize(), size.as_usize())? };
+    
+    let log = Log {
+        address: runner.address,
+        topics: vec![topic1],
+        data: log_data.clone(),
+    };
+    
+    runner.state.logs.push(log);
+
 
     if runner.debug_level.is_some() && runner.debug_level.unwrap() >= 1 {
         let data_hex = utils::debug::vec_to_hex_string(log_data);
@@ -131,12 +139,15 @@ pub fn log2(runner: &mut Runner) -> Result<(), ExecutionError> {
     let mut topic2 = [0u8; 32];
     raw_topic2.to_big_endian(&mut topic2);
 
-    // Check if the address is out of bounds
-    if offset.as_usize() + size.as_usize() > runner.memory.heap.len() {
-        return Err(ExecutionError::OutOfBoundsMemory);
-    }
-
     let log_data = unsafe { runner.memory.read(offset.as_usize(), size.as_usize())? };
+    
+    let log = Log {
+        address: runner.address,
+        topics: vec![topic1, topic2],
+        data: log_data.clone(),
+    };
+    
+    runner.state.logs.push(log);
 
     if runner.debug_level.is_some() && runner.debug_level.unwrap() >= 1 {
         let data_hex = utils::debug::vec_to_hex_string(log_data);
@@ -205,12 +216,15 @@ pub fn log3(runner: &mut Runner) -> Result<(), ExecutionError> {
     let mut topic3 = [0u8; 32];
     raw_topic3.to_big_endian(&mut topic3);
 
-    // Check if the address is out of bounds
-    if offset.as_usize() + size.as_usize() > runner.memory.heap.len() {
-        return Err(ExecutionError::OutOfBoundsMemory);
-    }
-
     let log_data = unsafe { runner.memory.read(offset.as_usize(), size.as_usize())? };
+    
+    let log = Log {
+        address: runner.address,
+        topics: vec![topic1, topic2, topic3],
+        data: log_data.clone(),
+    };
+    
+    runner.state.logs.push(log);
 
     if runner.debug_level.is_some() && runner.debug_level.unwrap() >= 1 {
         let data_hex = utils::debug::vec_to_hex_string(log_data);
@@ -292,12 +306,15 @@ pub fn log4(runner: &mut Runner) -> Result<(), ExecutionError> {
     let mut topic4 = [0u8; 32];
     raw_topic4.to_big_endian(&mut topic4);
 
-    // Check if the address is out of bounds
-    if offset.as_usize() + size.as_usize() > runner.memory.heap.len() {
-        return Err(ExecutionError::OutOfBoundsMemory);
-    }
-
     let log_data = unsafe { runner.memory.read(offset.as_usize(), size.as_usize())? };
+    
+    let log = Log {
+        address: runner.address,
+        topics: vec![topic1, topic2, topic3, topic4],
+        data: log_data.clone(),
+    };
+    
+    runner.state.logs.push(log);
 
     if runner.debug_level.is_some() && runner.debug_level.unwrap() >= 1 {
         let data_hex = utils::debug::vec_to_hex_string(log_data);
@@ -361,4 +378,87 @@ pub fn log4(runner: &mut Runner) -> Result<(), ExecutionError> {
 
     // Increment PC
     runner.increment_pc(1)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core_module::runner::Runner;
+    use crate::core_module::utils::bytes::{_hex_string_to_bytes, pad_left};
+    use crate::core_module::utils::errors::ExecutionError;
+
+    #[test]
+    fn test_log0() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("604260005260206000a0"), Some(2), true);
+        assert!(interpret_result.is_ok());
+
+        let log = runner.state.logs.get(0).unwrap();
+
+        assert!(log.topics.len() == 0);
+        assert!(log.address == runner.address);
+        assert!(log.data == pad_left(&[0x42]));
+    }
+
+    #[test]
+    fn test_log1() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("604260005260ff60206000a1"), Some(2), true);
+        assert!(interpret_result.is_ok());
+
+        let log = runner.state.logs.get(0).unwrap();
+
+        assert!(log.topics.len() == 1);
+        assert!(log.topics[0] == pad_left(&[0xff]));
+        assert!(log.address == runner.address);
+        assert!(log.data == pad_left(&[0x42]));
+    }
+
+    #[test]
+    fn test_log2() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("6042600052606060ff60206000a2"), Some(2), true);
+        assert!(interpret_result.is_ok());
+
+        let log = runner.state.logs.get(0).unwrap();
+
+        assert!(log.topics.len() == 2);
+        assert!(log.topics[0] == pad_left(&[0xff]));
+        assert!(log.topics[1] == pad_left(&[0x60]));
+        assert!(log.address == runner.address);
+        assert!(log.data == pad_left(&[0x42]));
+    }
+
+    #[test]
+    fn test_log3() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("604260005260ac606060ff60206000a3"), Some(2), true);
+        assert!(interpret_result.is_ok());
+
+        let log = runner.state.logs.get(0).unwrap();
+
+        assert!(log.topics.len() == 3);
+        assert!(log.topics[0] == pad_left(&[0xff]));
+        assert!(log.topics[1] == pad_left(&[0x60]));
+        assert!(log.topics[2] == pad_left(&[0xac]));
+        assert!(log.address == runner.address);
+        assert!(log.data == pad_left(&[0x42]));
+    }
+
+    #[test]
+    fn test_log4() {
+        let mut runner = Runner::_default(3);
+        let interpret_result: Result<(), ExecutionError> = runner.interpret(_hex_string_to_bytes("6042600052601d60ac606060ff60206000a4"), Some(2), true);
+        assert!(interpret_result.is_ok());
+
+        let log = runner.state.logs.get(0).unwrap();
+
+        assert!(log.topics.len() == 4);
+        assert!(log.topics[0] == pad_left(&[0xff]));
+        assert!(log.topics[1] == pad_left(&[0x60]));
+        assert!(log.topics[2] == pad_left(&[0xac]));
+        assert!(log.topics[3] == pad_left(&[0x1d]));
+        assert!(log.address == runner.address);
+        assert!(log.data == pad_left(&[0x42]));
+    }
+
 }
