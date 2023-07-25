07c3c77e8c4ba330152ea1238c5119917b468640
Léo FASANO <fasanol.info@gmail.com>
2023-07-25 10:42:37 +0200

    doc: add memory docs

--- src/core_module/memory.rs
+++ src/core_module/memory.rs
@@ -1,12 +1,22 @@
 use super::utils::errors::ExecutionError;
 use std::ptr;
 
+/// Represents the memory of the EVM.
 #[derive(Debug)]
 pub struct Memory {
     pub heap: Vec<u8>,
 }
 
 impl Memory {
+    /// Creates a new instance of `Memory`.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - An optional vector of bytes to initialize the memory with.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `Memory`.
     pub fn new(data: Option<Vec<u8>>) -> Self {
         Self {
             heap: if data.is_some() {
@@ -17,12 +27,25 @@ impl Memory {
         }
     }
 
-    // Extend memory by size
+    /// Extends the memory by the specified size.
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - The size to extend the memory by.
     pub fn extend(&mut self, size: usize) {
         self.heap.extend(vec![0; size]);
     }
 
-    // Load bytes from memory
+    /// Reads bytes from memory starting at the specified address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address to start reading from.
+    /// * `size` - The number of bytes to read.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the bytes read or an `ExecutionError` if the read operation failed.
     pub unsafe fn read(&mut self, address: usize, size: usize) -> Result<Vec<u8>, ExecutionError> {
         // Increase memory heap to the nearest multiple of 32 if address is out of bounds
         if address + size > self.heap.len() {
@@ -44,7 +67,16 @@ impl Memory {
         Ok(data)
     }
 
-    // Load bytes from memory
+    /// Writes bytes to memory starting at the specified address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address to start writing to.
+    /// * `data` - The bytes to write.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating whether the write operation was successful or an `ExecutionError` if it failed.
     pub unsafe fn write(&mut self, address: usize, data: Vec<u8>) -> Result<(), ExecutionError> {
         // check if memory should be extended
         if address + data.len() > self.heap.len() {
@@ -65,7 +97,15 @@ impl Memory {
         Ok(())
     }
 
-    // Load 32 bytes from memory
+    /// Reads 32 bytes from memory starting at the specified address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address to start reading from.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the 32 bytes read or an `ExecutionError` if the read operation failed.
     pub unsafe fn mload(&mut self, address: usize) -> Result<[u8; 32], ExecutionError> {
         // Increase memory heap to the nearest multiple of 32 if address is out of bounds
         if address + 32 > self.heap.len() {
@@ -84,7 +124,16 @@ impl Memory {
         Ok(ptr::read(ptr as *const [u8; 32]))
     }
 
-    // Store 32 bytes in memory
+    /// Writes 32 bytes to memory starting at the specified address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address to start writing to.
+    /// * `data` - The 32 bytes to write.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating whether the write operation was successful or an `ExecutionError` if it failed.
     pub unsafe fn mstore(&mut self, address: usize, data: [u8; 32]) -> Result<(), ExecutionError> {
         // Check if memory should be extended
         if address + 32 > self.heap.len() {
@@ -97,7 +146,11 @@ impl Memory {
         Ok(())
     }
 
-    // Get the memory size
+    /// Gets the size of the memory heap.
+    ///
+    /// # Returns
+    ///
+    /// The size of the memory heap.
     pub fn msize(&self) -> usize {
         self.heap.len()
     }
