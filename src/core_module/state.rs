725a5162aa16af2ac440df13b3435b6717957ad6
Léo FASANO <fasanol.info@gmail.com>
2023-07-25 13:43:04 +0200

    doc: add state docs

--- src/core_module/state.rs
+++ src/core_module/state.rs
@@ -14,13 +14,23 @@ use colored::*;
 /*                             AccountState struct                            */
 /* -------------------------------------------------------------------------- */
 
+/// Represents the state of an account on the Ethereum Virtual Machine.
 pub struct AccountState {
+    /// The account's nonce, which is incremented each time a transaction is sent from the account.
     pub nonce: u64,
+    /// The account's balance, represented as a 32-byte array.
     pub balance: [u8; 32],
+    /// The account's storage, represented as a hashmap where the keys and values are both 32-byte arrays.
     pub storage: HashMap<[u8; 32], [u8; 32]>,
+    /// The hash of the account's code, represented as a 32-byte array.
     pub code_hash: [u8; 32],
 }
 
+/// Implements the Debug trait for the AccountState struct, which allows for the struct to be printed in a formatted way.
+/// The function prints the nonce, balance, code hash, and storage of the account state.
+/// If the code hash is empty, it prints "Empty code" instead of the hash.
+/// For each storage slot and value, it prints them in a formatted way.
+/// If the storage is empty, it prints "Empty storage".
 impl fmt::Debug for AccountState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut code_hash: String = utils::debug::to_hex_string(self.code_hash);
@@ -60,12 +70,20 @@ impl fmt::Debug for AccountState {
 /*                                 Log struct                                 */
 /* -------------------------------------------------------------------------- */
 
+/// Represents a log entry in the Ethereum Virtual Machine (EVM) state.
 pub struct Log {
+    /// The address of the contract that generated the log.
     pub address: [u8; 20],
+    /// The topics associated with the log.
     pub topics: Vec<[u8; 32]>,
+    /// The data associated with the log.
     pub data: Vec<u8>,
 }
 
+/// Implements the Debug trait for the Log struct, which allows for the struct to be printed in a formatted way.
+/// The function writes the address, topics, and data of the Log struct to the provided formatter.
+/// If the topics vector is not empty, it prints each topic in a formatted way.
+/// Otherwise, it prints "No topics".
 impl fmt::Debug for Log {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(
@@ -102,16 +120,32 @@ impl fmt::Debug for Log {
 /*                              EVM state struct                              */
 /* -------------------------------------------------------------------------- */
 
+/// Represents the state of the Ethereum Virtual Machine (EVM).
 #[derive(Debug)]
 pub struct EvmState {
+    /// A mapping of account addresses to their respective account states.
     pub accounts: HashMap<[u8; 20], AccountState>,
+    /// A mapping of code hashes to their respective code.
     pub codes: HashMap<[u8; 32], Vec<u8>>,
+    /// A vector of logs generated during the execution of the EVM.
     pub logs: Vec<Log>,
+    /// A flag indicating whether the EVM is in static mode or not.
     pub static_mode: bool,
+    /// An optional provider for interacting with the Ethereum network.
     pub provider: Option<Provider<Http>>,
 }
 
+/// Implementation of the EVM state.
 impl EvmState {
+    /// Creates a new instance of the `State` struct.
+    ///
+    /// # Arguments
+    ///
+    /// * `fork_url` - An optional `String` representing the URL of the fork to use.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of the `State` struct.
     pub fn new(fork_url: Option<String>) -> Self {
         Self {
             accounts: HashMap::new(),
@@ -127,6 +161,26 @@ impl EvmState {
     }
 
     // Transfer value from one account to another
+    /// Transfers a given value from one account to another.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - An array of 20 bytes representing the address of the account to transfer from.
+    /// * `to` - An array of 20 bytes representing the address of the account to transfer to.
+    /// * `value` - An array of 32 bytes representing the value to transfer.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `ExecutionError` if:
+    ///
+    /// * The static mode is enabled (static call).
+    /// * The account to transfer from does not exist.
+    /// * The account to transfer to does not exist.
+    /// * The balance of the account to transfer from is insufficient.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Ok(())` if the transfer was successful.
     pub fn transfer(
         &mut self,
         from: [u8; 20],
@@ -180,6 +234,22 @@ impl EvmState {
         Ok(())
     }
 
+    /// Loads a 256-bit value from the storage of the given account at the given slot.
+    /// If the account is not found in the emulator's local state, the storage value is fetched from the provider.
+    /// If the provider is not set, or if the storage fetch fails, the function returns a zero-filled 256-bit value.
+    ///
+    /// # Arguments
+    ///
+    /// * `account` - An array of 20 bytes representing the address of the account to load from.
+    /// * `slot` - An array of 32 bytes representing the slot to load from.
+    ///
+    /// # Errors
+    ///
+    /// Never return an error, but returns a 32-byte array of zero instead.
+    ///
+    /// # Returns
+    ///
+    /// Returns a 32-byte array representing the value at the given slot.
     pub fn sload(&mut self, account: [u8; 20], slot: [u8; 32]) -> Result<[u8; 32], ExecutionError> {
         match self.accounts.get(&account) {
             Some(account_state) => match account_state.storage.get(&slot) {
@@ -222,6 +292,17 @@ impl EvmState {
     }
 
     // Store a 32 bytes word in storage of a specific account
+    /// Stores a value in the storage of an account.
+    ///
+    /// # Arguments
+    ///
+    /// * `account` - The address of the account to store the value in.
+    /// * `slot` - The slot in the storage to store the value in.
+    /// * `value` - The value to store in the specified slot.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `ExecutionError` if the static mode is enabled or if the account is not found.
     pub fn sstore(
         &mut self,
         account: [u8; 20],
@@ -242,7 +323,19 @@ impl EvmState {
         }
     }
 
-    // Get the code of an account
+    /// Returns the code at the given address. If the code is not already in the state, it will be fetched from the blockchain using the provider.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - The address of the contract to get the code for.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `ExecutionError` if the code is not found.
+    ///
+    /// # Returns
+    ///
+    /// Returns a reference to the Vec<u8> of the code.
     pub fn get_code_at(&mut self, address: [u8; 20]) -> Result<&Vec<u8>, ExecutionError> {
         match self.accounts.get(&address) {
             Some(account_state) => {
@@ -281,7 +374,20 @@ impl EvmState {
         }
     }
 
-    // Store the code of an account
+    /// Stores the code of an account at the given address.
+    ///
+    /// # Arguments
+    ///
+    /// * `address` - A 20-byte array representing the address of the account.
+    /// * `code` - A vector of bytes representing the code to be stored.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `ExecutionError` if the account is not found.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Ok(())` if the code is successfully stored.
     pub fn put_code_at(&mut self, address: [u8; 20], code: Vec<u8>) -> Result<(), ExecutionError> {
         let code_hash = self.put_code(code)?;
 
@@ -294,14 +400,26 @@ impl EvmState {
         }
     }
 
-    // Load contract code
+    /// Returns a reference to the code associated with the given code hash.
+    ///
+    /// # Arguments
+    ///
+    /// * `code_hash` - A 32-byte array representing the hash of the code to retrieve.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `ExecutionError::CodeNotFound` error if the code hash is not found in the state.
+    ///
+    /// # Returns
+    ///
+    /// Returns a reference to the code associated with the given code hash.
     fn get_code(&self, code_hash: [u8; 32]) -> Result<&Vec<u8>, ExecutionError> {
         self.codes
             .get(&code_hash)
             .ok_or(ExecutionError::CodeNotFound)
     }
 
-    // Store contract code and return its hash
+    /// Store contract code and return its hash
     fn put_code(&mut self, code: Vec<u8>) -> Result<[u8; 32], ExecutionError> {
         // Check if static mode is enabled
         if self.static_mode {
@@ -313,6 +431,8 @@ impl EvmState {
         Ok(code_hash)
     }
 
+    /// Print the state of the EVM
+    /// This function is used for debugging purposes.
     pub fn debug_state(&mut self) {
         let border_line =
             "\n╔═══════════════════════════════════════════════════════════════════════════════════════════════════════╗";
