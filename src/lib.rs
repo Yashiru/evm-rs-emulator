b2b5ceb3cf36f09f874070f42454cefdbb80f311
Léo FASANO <fasanol.info@gmail.com>
2023-09-20 16:38:22 +0200

    feat: improve lib.rs exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 mod core_module;
+
+/* ---------------------------------- Core ---------------------------------- */
 pub use core_module::memory::Memory;
+pub use core_module::op_codes;
 pub use core_module::runner::Runner;
 pub use core_module::stack::Stack;
 pub use core_module::state::EvmState;
+
+/* ---------------------------------- Utils --------------------------------- */
+pub use core_module::utils::bytes;
+pub use core_module::utils::debug;
+pub use core_module::utils::environment;
+pub use core_module::utils::errors;
